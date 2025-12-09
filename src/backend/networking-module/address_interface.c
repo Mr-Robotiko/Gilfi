ea2338d69a80cbccb619c3cd0ca9e9c5003eca50
Marco Keiditsch <marcokeiditsch@hotmail.com>
2025-12-09 12:47:16 +0100

    Fixed address_interface crash due to wrong parameters

--- src/backend/networking-module/address_interface.c
+++ src/backend/networking-module/address_interface.c
@@ -7,7 +7,7 @@
 #include <netinet/in.h>
 #include "address_interface.h"
 
-int resolve_host(char *hostname, struct sockaddr_in *ipv4, struct sockaddr_in6 *ipv6)
+int resolve_host(char *hostname, struct sockaddr_in **ipv4, struct sockaddr_in6 **ipv6)
 {
     struct addrinfo *resolved, *iterating_pointer;
     int counter = 0;
@@ -18,23 +18,23 @@ int resolve_host(char *hostname, struct sockaddr_in *ipv4, struct sockaddr_in6 *
 
     status = getaddrinfo(hostname, NULL, &hints, &resolved);
 
-    if(status != 0)
+    if                                                                          (status != 0)
     {
         return status;
     }
 
-    ipv4 = NULL;
-    ipv6 = NULL;
+    *ipv4 = NULL;
+    *ipv6 = NULL;
     
     for(iterating_pointer = resolved; iterating_pointer != NULL; iterating_pointer = iterating_pointer->ai_next)
     {
         if(iterating_pointer->ai_family == AF_INET)
         {
-            ipv4 = (struct sockaddr_in *)iterating_pointer->ai_addr;
+            *ipv4 = (struct sockaddr_in *)iterating_pointer->ai_addr;
         } 
-        else 
+        else if(iterating_pointer->ai_family == AF_INET6)
         {
-            ipv6 = (struct sockaddr_in6 *)iterating_pointer->ai_addr;
+            *ipv6 = (struct sockaddr_in6 *)iterating_pointer->ai_addr;
         }
     }
 
@@ -46,12 +46,14 @@ int main()
 {
     memset(&hints, 0, sizeof hints);
 
+    //Testing Code:
+
     struct sockaddr_in *ipv4;
     struct sockaddr_in6 *ipv6;
 
     char ip_string[INET6_ADDRSTRLEN];
 
-    int status = resolve_host("www.youtube.com", ipv4, ipv6);
+    int status = resolve_host("thepiratebay.org", &ipv4, &ipv6);
 
     if(status != 0)
     {
